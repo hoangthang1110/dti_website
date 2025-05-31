865847b8db0e04b117bfbdd7b5e38aad7e437210
hoangthang1110 <hoangthang1110@gmail.com>
2025-05-31 08:26:07 +0700

    Add files via upload

--- dti_dashboard/apps.py
+++ dti_dashboard/apps.py
@@ -1,6 +1,14 @@
+# dti_website/dti_dashboard/apps.py
 from django.apps import AppConfig
-
 
 class DtiDashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dti_dashboard'
+    # Bạn KHÔNG cần method ready() nếu không có signals tùy chỉnh.
+    # Nếu có method ready(), đảm bảo nó không gây lỗi.
+    # Nếu bạn không chắc chắn, hãy comment hoặc xóa nó đi.
+    # def ready(self):
+    #     try:
+    #         import dti_dashboard.signals
+    #     except ImportError:
+    #         pass
